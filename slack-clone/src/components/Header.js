69a0a2b3a27d4071e9350ec846a3605374b60b14
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-24 00:17:09 +0530

    ADD: header => headerSearch

--- slack-clone/src/components/Header.js
+++ slack-clone/src/components/Header.js
@@ -2,18 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 import Avatar from '@material-ui/core/Avatar';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 function Header() {
     return (
       <HeaderContainer>
           {/* Header left  */}
+
+
           <HeaderLeft>
              <HeaderAvatar 
                // TODO: Add onClick
               />
               <AccessTimeIcon />
           </HeaderLeft>
+
+
           {/* Header search  */}
+
+          <HeaderSearch>
+             <SearchIcon />
+             <input placeholder="Search Slack Clone" />
+          </HeaderSearch>
 
           {/* header right */}
       </HeaderContainer>
@@ -31,6 +42,27 @@ const HeaderContainer = styled.div `
   padding :10px 0 ;
   justify-content: space-between;
   color:  white;
+`;
+
+const HeaderSearch = styled.div `
+  flex: 0.4 ;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: grey;
+  border: 1px grey solid ;
+
+  > input {
+    background-color : transparent;
+    border: none ;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color : white;
+  }
 `;
  
 const HeaderLeft = styled.div `
