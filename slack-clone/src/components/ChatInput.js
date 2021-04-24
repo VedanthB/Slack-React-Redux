9d2ac85ad45379333ff0019f1d9b7bf6026c9cb1
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-25 05:23:22 +0530

    ADD: add messages of the room to firebase

--- slack-clone/src/components/ChatInput.js
+++ slack-clone/src/components/ChatInput.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Button from '@material-ui/core/Button';
+import { db } from '../firebase';
+import firebase from 'firebase'
+import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
 
 
-function ChatInput({ channelid , channelName }) {
+function ChatInput({ channelId , channelName }) {
+    const [input, setInput] = useState('')
+
     const sendMessage = (e) => {
         e.preventDefault() ;
         
+        if (!channelId) {
+            return false  // exit the function if we dont have channel id
+        }
+
+      // adds to db in rooms 
+        db.collection('rooms').doc(channelId).collection('messages').add({
+            message:  input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            user: 'Supertramp',
+            userImage: 'https://png.pngtree.com/png-clipart/20191120/original/pngtree-pink-watercolor-brushes-png-image_5054156.jpg',
+        })
+
+        setInput('')
     }
-    
+
 
     return (
         <ChatInputContainer>
-            <form action='' >
-                <input placeholder={`Message #Room`} />
+            <form >
+                <input onChange={(e) => setInput(e.target.value)} value={input} placeholder={`Message #Room`} />
 
-                <Button type='submit' hidden onClick={sendMessage} >
+                <Button  type='submit' hidden onClick={sendMessage} >
                     send
                 </Button>
             </form>
